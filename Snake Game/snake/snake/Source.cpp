a4f23beca86ec66c678d8a49ce73a09e73870439
randyramadhan1973 <randyramadhan1973@gmail.com>
2019-05-26 05:18:35 +0700

    membuat function

---
+++ Snake Game/snake/snake/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//membuat 4 function : Setup, Draw, Input, logic 
+void setup() {
+
+}
+void draw() {
+
+}
+void input() {
+
+}
+void logic() {
+
+}
+
+int main() {
+
+
+	return 0;
+}
