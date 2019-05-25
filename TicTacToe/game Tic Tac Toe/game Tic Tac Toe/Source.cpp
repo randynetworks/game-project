9b0b0c4a6123b45a44385ebb8535efec3fde7af3
randyramadhan1973 <randyramadhan1973@gmail.com>
2019-05-25 18:53:01 +0700

    menampilkan tic tac toe Void Draw

--- TicTacToe/game Tic Tac Toe/game Tic Tac Toe/Source.cpp
+++ TicTacToe/game Tic Tac Toe/game Tic Tac Toe/Source.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+char matrix[3][3] = { '1','2','3','4','5','6','7','8','9' };
+
+
+// menampilkan TIc Tac Toe
+void Draw() {
+
+	cout << "Tic Tac Toe v1.0" << endl;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+
 int main() {
 
+	Draw();
+	system("pause");
 
 	cin.get();
 	return 0;
